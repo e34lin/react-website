3cd527de676af3b0d1258983974ad02f513768c8
Elizabeth Lin <elizabethlin@Elizabeths-MacBook-Pro.local>
2020-05-25 22:42:43 -0400

    resume

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 
+import resume from "../pics/myresume.pdf";
+
 class Resume extends React.Component {
 
     render()  {
@@ -10,7 +12,7 @@ class Resume extends React.Component {
         return(
             <Container fluid={true}>
                 <Row className="justify-content-around">
-                    <object data='myresume.pdf' type="application/pdf" width='750' height='1020'>My Resume Displayed</object>
+                    <object src={resume} width='750' height='1020'>My Resume Displayed</object>
                 </Row>
             </Container>
 
